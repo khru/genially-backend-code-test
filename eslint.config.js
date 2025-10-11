82592602bc2333c527b3e80b01116b046fa5cf31
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2025-10-11 20:54:11 +0200

    ci: linting

---
+++ eslint.config.js
@@ -0,0 +1,35 @@
+import tseslint from "typescript-eslint";
+
+export default [
+  ...tseslint.configs.recommended,
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      ecmaVersion: 2018,
+      sourceType: "module",
+    },
+    rules: {
+      semi: ["error", "always"],
+      quotes: ["error", "double"],
+      "@typescript-eslint/explicit-function-return-type": "off",
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-inferrable-types": [
+        "warn",
+        {
+          ignoreParameters: true,
+        },
+      ],
+      "@typescript-eslint/no-unused-vars": "warn",
+    },
+  },
+  {
+    ignores: [
+      "node_modules/**",
+      "dist/**",
+      "coverage/**",
+      "**/*.d.ts",
+      "src/public/**",
+      "src/types/**"
+    ],
+  },
+];
