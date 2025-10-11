117d7c71c7fd67d93883c2604864cf62e75411c9
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2025-10-11 12:35:08 +0200

    feat: create new genially working

--- src/api/controllers/create-genially.ts
+++ src/api/controllers/create-genially.ts
@@ -6,9 +6,25 @@ import Genially from "../../contexts/core/genially/domain/Genially";
 const geniallyRepository = new InMemoryGeniallyRepository();
 const createGeniallyService = new CreateGeniallyService(geniallyRepository);
 
+type CreateGeniallyResponse = {
+  id: string;
+  name: string;
+  description: string;
+  createdAt: string;
+  modifiedAt: string;
+  deletedAt: string;
+}
 export const execute = async (request: Request, response: Response) => {
   const genially: Genially = await createGeniallyService.execute(request.body);
+  const geniallyResponse: CreateGeniallyResponse = {
+    id: genially.id,
+    name: genially.name,
+    description: genially.description,
+    createdAt: genially.createdAt.toISOString(),
+    modifiedAt: genially.modifiedAt?.toISOString() || null,
+    deletedAt: genially.deletedAt?.toISOString() || null
+  };
   response.status(201).contentType("application/json").send(
-    genially
+    geniallyResponse
   );
 };
