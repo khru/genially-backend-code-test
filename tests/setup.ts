23963bc349a55e66083af10d64a46535428cde41
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2025-10-13 14:14:30 +0200

    test: removing console log silence on test

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,10 +1,3 @@
-beforeAll(() => {
-  jest.spyOn(console, "log").mockImplementation(() => {});
-  jest.spyOn(console, "info").mockImplementation(() => {});
-  jest.spyOn(console, "warn").mockImplementation(() => {});
-  jest.spyOn(console, "error").mockImplementation(() => {});
-});
-
 afterAll(async () => {
   jest.restoreAllMocks();
   jest.clearAllMocks();
