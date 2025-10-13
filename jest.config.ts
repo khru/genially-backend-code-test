dc8d205b227c5d09bf887daa010dfad23b93b6f0
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2025-10-13 10:07:40 +0200

    chore: remove console from test on setup and add awilix

--- jest.config.ts
+++ jest.config.ts
@@ -7,6 +7,7 @@ const config: Config = {
   preset: "ts-jest",
   testEnvironment: "node",
   roots: ["<rootDir>/src", "<rootDir>/tests"],
+  setupFilesAfterEnv: ["<rootDir>/tests/setup.ts"],
   testMatch: ["<rootDir>/tests/**/*.test.ts", "<rootDir>/tests/**/*.spec.ts"],
   collectCoverageFrom: ["src/**/*.ts", "!src/**/*.d.ts", "!src/api/server.ts"],
   coverageDirectory: "coverage",
